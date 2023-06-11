766ec05f99e2a568901772f94b6d95480a5af816
Pijar <pijarpahlawanq@students.amikom.ac.id>
2023-06-12 06:52:46 +0700

    remove duplicated file

--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -1,12 +1,13 @@
 import style from "../assets/css/ErrorPage.module.css";
-import Logo from "../components/Logo";
 
 export default function ErrorPage() {
   return (
     <div className={style.errorPage}>
-      <Logo extendedClass={style.extendedLogo} />
-      <h1>404</h1>
-      <p>Halaman tidak ditemukan</p>
+      <div className={style.container}>
+        <p>
+          <span>4</span>0<span>4</span> Error not found
+        </p>
+      </div>
     </div>
   );
 }
