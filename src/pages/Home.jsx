9d8fc2ed96584047bc1761650c20516674cf377a
MeyTeplon <ASUS@LAPTOP-VTDSD35M>
2023-07-08 15:18:02 +0700

    kel 3

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,17 +2,31 @@
 import style from '../assets/css/Home.module.css'
 import { FaCartArrowDown } from "react-icons/fa";
 import Card from '../components/Card';
+import { Router } from 'react-router-dom';
 export default function Home() {
   return (<main>
-    <div className={style.category}>
-      <Card/>
-      <Card/>
-      <Card/>
-      <Card/>
-
+    <section className={style.body}>
+          <div className={style.atas}>
+            {/* iklan diatas  */}
+              contoh saja 
+            <div className={style.kiri}>
+              contoh saja
+              {/* contoh saja */}
+            </div>
+          </div>
+          <div className={style.bawah}>
+            {/* iklan dibawah */}
+            contoh saja
+          </div>
+    </section>
+    <section className={style.footer}>
+      {/* berisi footer */}
+      contoh saja 
+    </section>
       
-    </div>
     
 </main>); 
   
 }
+
+
